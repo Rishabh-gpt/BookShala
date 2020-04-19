1c293c4ae0a045fc9db3b64f663f13fd339b1d2a
EC2 Default User <ec2-user@ip-172-31-37-72.ap-south-1.compute.internal>
2020-04-19 15:15:07 +0000

    add mongoDB

--- app.js
+++ app.js
@@ -12,7 +12,15 @@ var express = require("express"),
     seedDB  =require("./seeds");
     // seedDB();
     var middleware=require("./middleware");
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb+srv://rishabh:rusty@cluster0-rvyxk.mongodb.net/test?retryWrites=true&w=majority",{
+	useNewUrlParser: true,
+	useCreateIndex: true
+}).then(() => {
+	console.log('Connected to DB!');
+}).catch(err => {
+	console.log('ERROR:', err.message);
+});
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
